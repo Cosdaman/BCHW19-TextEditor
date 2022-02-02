b1579ad729865f69d31fc1a37f07310032c26a9c
Cosdaman <Cosdaguy@gmail.com>
2022-02-01 21:37:26 -0800

    added indexeddb

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -7,22 +7,40 @@ const initdb = async () =>
         console.log('jate database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: false });
       console.log('jate database created');
     },
   });
 
 export const putDb = async (content) => {
-  console.log(content)
-  const jateDb = await openDB('jate', 1);
-  const tx = jateDb.transaction('jate', 'readwrite');
-  const store = tx.objectStore('jate');
-  const request = store.put({ content: content })
-  const result = await request;
-  console.log('🚀 - data saved to the database', result);
+  try {
+    console.log(content)
+    const jateDb = await openDB('jate', 1);
+    const tx = jateDb.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    const request = store.put({ content: content, id: 1})
+    const result = await request;
+    console.log('data saved to the database', result);
+  }
+  catch {
+    console.error('put failed')
+  }
 }
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  try {
+    const jateDb = await openDB('jate', 1);
+    const tx = jateDb.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
+    const request = store.getAll();
+    const result = await request;
+    console.log('result.value', result[0].content);
+    return result[0].content;
+  }
+  catch {
+    console.error('get failed')
+  }
+
+}
 
 initdb();
